26b702bb6f6d5b0c8c90086ad6f9638083a69201
oliver15022003 <oliver.mann007@gmail.com>
2024-05-08 11:23:21 +0200

    [Senpai_frontend] added react template

---
+++ senpai_frontend/src/App.js
@@ -0,0 +1,11 @@
+import StartPage from "./pages/startPage/StartPage.js";
+
+function App() {
+  return (
+    <>
+    <StartPage></StartPage>
+    </>
+  );
+}
+
+export default App;
---
+++ senpai_frontend/src/pages/startPage/StartPage.js
@@ -0,0 +1,9 @@
+
+function StartPage(){
+    return(
+        <h1>
+            Hello World
+        </h1>
+    );
+}
+export default StartPage;
