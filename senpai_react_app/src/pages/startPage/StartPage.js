f44e785cd002d8bf7e396f57253873cb381db920
oliver15022003 <oliver.mann007@gmail.com>
2024-05-10 15:54:28 +0200

    [Startpage Background]changed the startpage background to an ai generated JPG

--- senpai_react_app/src/pages/startPage/StartPage.js
+++ senpai_react_app/src/pages/startPage/StartPage.js
@@ -1,5 +1,7 @@
 import styles from '../General.module.css';
+import styles2 from "./StartPage.module.css";
 import { useNavigate } from "react-router-dom";
+import senpaiBackground from "../../assets/senpaiStartPage.jpg";
 
 function StartPage(){
     // useNavigate is used to switch between pages
@@ -10,17 +12,18 @@ function StartPage(){
     };
 
     return(
-        <>
-        <h1>
-           Senpai 
-        </h1>
-        <div>
-            <button className = {styles.button01} 
-                    onClick={handleClick}>
-                Start Your Journey</button>
+        <div className={styles2.background} style={{backgroundImage: `url(${senpaiBackground})`}}>
+            <div className= {styles2.startDiv}>
+                <h1>Senpai</h1>
+                <div>
+                    <button className={styles.button01} onClick={handleClick}>
+                        Start Your Journey
+                    </button>
+                </div>
+            </div>
+            
         </div>
-        </>
-        
     );
 }
+
 export default StartPage;
