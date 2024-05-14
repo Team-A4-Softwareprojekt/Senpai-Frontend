3e0e343f91e5985ec36f2fac31afb87494a2d7e0
oliver15022003 <oliver.mann007@gmail.com>
2024-05-14 22:02:01 +0200

    [Styled Account Page] Added better styling and a UserCard Component to the AccountSettingsPage

--- src/pages/accountSettingsPage/AccountSettingsPage.jsx
+++ src/pages/accountSettingsPage/AccountSettingsPage.jsx
@@ -1,32 +1,55 @@
 import styles from '../General.module.css';
 import styles2 from './AccountSettingsPage.module.css';
+import UserCard from '../../components/userCard/UserCard.jsx';
 
 function AccountSettingsPage() {
     return (
         <>
-        <div className={styles2.userInfo}>
-            <p>Name: Max Mustermann</p>
-            <p>Mail: max.mustermann@gmail.com</p>
-            <p>Streak: 20 </p>
-            <button className={styles.button01}>Courses</button>
-            <button className={styles.button01}>Corrected Exercises</button>
-        
-            <div className={styles2.friendList}>
-            <input type="text" placeholder="Search friends" />
-                <div className={styles2.friend}>
-                    <p>Username: John Doe</p>
-                    <p>Streak: 10</p>
-                </div>
-                <div className={styles2.friend}>
-                    <p>Username: Jane Smith</p>
-                    <p>Streak: 5</p>
-                </div>
-                <div className={styles2.friend}>
-                    <p>Username: Alex Johnson</p>
-                    <p>Streak: 15</p>
-                </div>
+        <div className={styles2.accountDiv}>
+            <div className= {styles2.userDiv}>
+                <p>Name: Max Mustermann</p>
+                <p>Mail: max.mustermann@gmail.com</p>
+                <p>Streak: 20 </p>
+                <button className={styles.button01}>Courses</button>
+                <button className={styles.button01}>Corrected Exercises</button>
+                
             </div>
-            <footer>
+            <input type="text" placeholder="Search friends" />
+            <div className={styles2.friendList}>
+                
+                <UserCard 
+                    userName = "Jane Doe" 
+                    userEmail= "jane.doe@gmail.com"
+                    userStreak= "44">
+                </UserCard>
+                <UserCard 
+                    userName="John Smith" 
+                    userEmail="john.smith@gmail.com"
+                    userStreak="30"
+                />
+                <UserCard 
+                    userName="Alice Johnson" 
+                    userEmail="alice.johnson@gmail.com"
+                    userStreak="15"
+                />
+                <UserCard 
+                    userName="Bob Anderson" 
+                    userEmail="bob.anderson@gmail.com"
+                    userStreak="10"
+                />
+                <UserCard 
+                    userName="Bob Anderson" 
+                    userEmail="bob.anderson@gmail.com"
+                    userStreak="10"
+                />
+                <UserCard 
+                    userName="Bob Anderson" 
+                    userEmail="bob.anderson@gmail.com"
+                    userStreak="10"
+                />
+                
+            </div>
+            <footer className= {styles2.footerButtons}>
                 <div className={styles2.buttons}>
                     <button className={styles.button01}>Change Password</button>
                     <button className={styles.button01}>Change Abbonnement</button>
