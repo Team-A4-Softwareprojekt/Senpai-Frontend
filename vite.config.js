dc40c61225af4b3709d9bd1c4847e689a476befc
Tobi511 <tobi.wegner@gmx.de>
2024-05-20 19:57:19 +0200

    [FIX] edit vite.config.js to support .jsx

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Senpai-Frontend/"
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    mimeTypes: {
+      '.jsx': 'text/javascript',
+    },
+  },
+});
