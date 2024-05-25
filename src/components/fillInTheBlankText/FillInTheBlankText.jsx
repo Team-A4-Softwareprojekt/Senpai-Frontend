26efe457106233fa31b9f5ffc6aa726394a1ebeb
oliver15022003 <oliver.mann007@gmail.com>
2024-05-25 15:50:57 +0200

    [Feature] Added a modal with an explanation to the FillInTheBlank component

--- src/components/fillInTheBlankText/FillInTheBlankText.jsx
+++ src/components/fillInTheBlankText/FillInTheBlankText.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import styles from '../../pages/General.module.css';
+import styles2 from './FillInTheBlankText.module.css';
+import Modal from '../modal/Modal.jsx';
 
 // FillInTheBlankText component
 function FillInTheBlankText({ text, blankIndices }) {
   const [inputs, setInputs] = useState(Array(blankIndices.length).fill(''));
   const [words, setWords] = useState(text.split(' '));
-  const [show, setShow] = useState(Array(blankIndices.length).fill(false));
-  const [correct, setCorrect] = useState(Array(blankIndices.length).fill(false));
-  const [incorrect, setIncorrect] = useState(Array(blankIndices.length).fill(false));
+  const [show, setShow] = useState(false);
+  const [results, setResults] = useState(Array(blankIndices.length).fill(false));
 
   // Handle input change
   const handleChange = (e, idx) => {
@@ -17,29 +18,14 @@ function FillInTheBlankText({ text, blankIndices }) {
   };
 
   // Handle check button click
-  const handleCheck = (idx) => {
-    const newShow = [...show];
-    const newCorrect = [...correct];
-    const newIncorrect = [...incorrect];
-
-    // Check if the input is correct
-    if (inputs[idx] === words[blankIndices[idx]]) {
-      newShow[idx] = true;
-      newCorrect[idx] = true;
-      newIncorrect[idx] = false;
-    } else {
-      newShow[idx] = true;
-      newCorrect[idx] = false;
-      newIncorrect[idx] = true;
-    }
-
-    setShow(newShow);
-    setCorrect(newCorrect);
-    setIncorrect(newIncorrect);
+  const handleCheck = () => {
+    const newResults = blankIndices.map((index, idx) => inputs[idx] === words[index]);
+    setShow(true);
+    setResults(newResults);
   };
 
   return (
-    <div>
+    <div className={styles2.FillInTheBlankTextDiv}>
       <p>
         {words.map((word, index) => {
           if (blankIndices.includes(index)) {
@@ -51,11 +37,11 @@ function FillInTheBlankText({ text, blankIndices }) {
                   value={inputs[blankIdx]}
                   onChange={(e) => handleChange(e, blankIdx)}
                   style={{
-                    backgroundColor: show[blankIdx] && correct[blankIdx]
-                      ? 'lightgreen'
-                      : show[blankIdx] && incorrect[blankIdx]
-                        ? 'lightcoral'
-                        : 'transparent',
+                    backgroundColor: show
+                      ? results[blankIdx]
+                        ? 'lightgreen'
+                        : 'lightcoral'
+                      : 'transparent',
                     color: 'white',  // Set the text color to white
                   }}
                 />{' '}
@@ -66,12 +52,16 @@ function FillInTheBlankText({ text, blankIndices }) {
           }
         })}
       </p>
-      {blankIndices.map((_, idx) => (
-        <div key={idx}>
-          <button onClick={() => handleCheck(idx)} className = {styles.button01}>Check {idx + 1}
-          </button>
+      <div className={styles2.buttonDiv}>
+        <button onClick={handleCheck} className={styles.button01}>
+          Check
+        </button>
+        <div className = {styles2.modal}>
+          <Modal header = 'Fill In The Blank Text' 
+            text = 'Please fill in the blanks with the correct words. Once you have filled in all the blanks, click the Check button to see if your answers are correct. Correct answers will be highlighted in green, while incorrect answers will be highlighted in red.' > </Modal>
         </div>
-      ))}
+        
+      </div>
     </div>
   );
 }
