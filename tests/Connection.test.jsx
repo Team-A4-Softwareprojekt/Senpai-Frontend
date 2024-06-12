3fa38d9ac6272c94da4e9c77a7cfacaec89a7aa9
Tobi511 <tobi.wegner@gmx.de>
2024-06-12 10:32:02 +0200

    [FIX] Connection Test only checks the current socket URL now

--- tests/Connection.test.jsx
+++ tests/Connection.test.jsx
@@ -7,6 +7,7 @@ test('the URL should be the Render URL', () => {
     expect(URL).toBe('https://senpai-development.onrender.com/');
 })
 
+/*
 test('should establish socket connection and receive CONNECTION_TEST_SUCCESSFULLY', () => {
     //socket.connect();
     socket.emit('CONNECTION_TEST');
@@ -18,7 +19,8 @@ test('should establish socket connection and receive CONNECTION_TEST_SUCCESSFULL
 })
 
 test('should return true for valid test data', async () => {
-    const response = await fetch('http://localhost:3000/connection_test?data=TEST');
+    //const response = await fetch('http://localhost:3000/connection_test?data=TEST');
+    const response = await fetch('https://senpai-development.onrender.com/connection_test?data=TEST');
     const data = await response.json();
 
     expect(response.status).toBe(200);
@@ -26,9 +28,12 @@ test('should return true for valid test data', async () => {
 });
 
 test('should return false for invalid test data', async () => {
-    const response = await fetch('http://localhost:3000/connection_test?data=INVALID');
+    //const response = await fetch('http://localhost:3000/connection_test?data=INVALID');
+    const response = await fetch('https://senpai-development.onrender.com/connection_test?data=INVALID');
     const data = await response.json();
 
     expect(response.status).toBe(200);
     expect(data).toBe(false);
 });
+*/
+
