b5a89edabc1e0ec3dd558e36e978e9b5f036b8e2
kzuber93 <KevinZuber93@gmail.com>
2024-06-20 18:59:58 +0200

    [FEATURE] Added PopUpMissingContent

--- src/pages/codeSenpaiPage/CodeSenpaiPage.jsx
+++ src/pages/codeSenpaiPage/CodeSenpaiPage.jsx
@@ -1,5 +1,4 @@
-import styles from '../General.module.css';
-import styles2 from './CodeSenpaiPage.module.css';
+import styles from './CodeSenpaiPage.module.css';
 import SelectContentCard from '../../components/selectContentCard/SelectContentCard.jsx';
 import codeChallengeImg from '../../assets/codeChallenge.jpg';
 import codeBattleImg from '../../assets/codeBattle.jpg';
@@ -9,7 +8,7 @@ import HomeButton from '../../components/homeButton/HomeButton';
 import AccountButton from '../../components/accountButton/AccountButton';
 import ChangeTopicButton from '../../components/changeTopicButton/ChangeTopicButton';
 import { useNavigate } from 'react-router-dom';
-import {socket,requestQuestion, requestDailyChallengeQuestion} from '../../socket.js';
+import {socket, requestDailyChallengeQuestion} from '../../socket.js';
 
 /*
 This is the code-senpai page that holds an account button, amount of lives and three different
@@ -60,14 +59,14 @@ function codeSenpaiPage() {
             <h1>
                 Choose your form of training
             </h1>
-            <div className= {styles2.cardsGridContainer}>      
+            <div className= {styles.cardsGridContainer}>      
                 <SelectContentCard 
                     buttonText= "Daily Challenge"
                     imageUrl={codeChallengeImg}
                     /*linkTo={"/select/code/dailyChallenge"}*/
                     modalHeader= "Daily Challenge" 
                     modalText = "Participate in daily challenges to improve your skills and build a continuous learning routine."
-                    className= {styles2.selectCard}
+                    className= {styles.selectCard}
                     handleClick = {handleDailyChallenge}
                     />
                     	
@@ -77,7 +76,7 @@ function codeSenpaiPage() {
                     linkTo={"/select/code/codeBattle"}
                     modalHeader = "Code Battle" 
                     modalText = "Challenge yourself and compete with others in entertaining code duels to expand your knowledge in a playful way."
-                    className= {styles2.selectCard}/>
+                    className= {styles.selectCard}/>
                     
                 <SelectContentCard 
                     buttonText= "Exercise"
@@ -85,7 +84,7 @@ function codeSenpaiPage() {
                     linkTo={"/select/code/exercise"}
                     modalHeader= "Exercise" 
                     modalText= "Work on complex tasks in different categories and receive individual feedback from experienced tutors to deepen your understanding."
-                    className= {styles2.selectCard}/>
+                    className= {styles.selectCard}/>
             </div>
             <HomeButton handleClick={handleHomeClick} />
             <AccountButton handleClick={handleAccountClick} />
