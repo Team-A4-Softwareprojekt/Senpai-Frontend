315940a03e8382646b3269d29a5e5ede52c0d3fa
LewAnWa <lanzekk@gmail.com>
2024-07-01 13:03:03 +0200

    [Fix] Resetting manipulation points when game is over

--- src/popups/popUpGameLoser/PopUpGameLoser.jsx
+++ src/popups/popUpGameLoser/PopUpGameLoser.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
 import animationData from '../../animations/LoserAnimation.json';
 import styles from './PopUpGameLoser.module.css';
@@ -6,15 +6,19 @@ import ConfirmButton from '../../buttons/confirmButton/ConfirmButton';
 import { useNavigate } from 'react-router-dom';
 import { socket } from '../../socket.js';
 import ScoresFinal from '../../components/scoresFinal/ScoresFinal';
+import {ScoreContext} from "../../context/scoreContext.jsx";
 
 const PopUpGameLoser = ({ loser, isVisible, ownPoints, opponentPoints, resetRoundCounter }) => {
   const navigate = useNavigate();
+  const {setOwnPoints, setOpponentPoints} = useContext(ScoreContext);
 
   if (!isVisible) {
     return null;
   }
 
   const handleLoserConfirm = () => {
+    setOwnPoints(0);
+    setOpponentPoints(0);
     socket.emit('CLOSE_LOBBY');
     navigate('/select/code/codeBattle');
     resetRoundCounter();
