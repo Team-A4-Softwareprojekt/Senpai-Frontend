360e6e2b29e19b208c5294a8ed34239a00f38b67
kzuber93 <KevinZuber93@gmail.com>
2024-07-03 21:01:53 +0200

    [UPDATE] Cleaned up General.module.css and added information text in StartPage

--- src/pages/startPage/StartPage.jsx
+++ src/pages/startPage/StartPage.jsx
@@ -18,10 +18,12 @@ function StartPage() {
                     </button>
                 </div>
                 <div className={styles.infoBox}>
-                    Welcome to Senpai. Begin your journey by clicking the button on the right.
-                    Welcome to Senpai. Begin your journey by clicking the button on the right.
-                    Welcome to Senpai. Begin your journey by clicking the button on the right.
-                    Welcome to Senpai. Begin your journey by clicking the button on the right.
+                    <li>Du interessierst dich für die Themenbereiche Programmieren, Mathematik, Medizin oder Fitness und möchtest mehr dazulernen?<br/></li>
+                    <li>Du bist traditionelle Lernmethoden leid?<br/></li>
+                    <li>Du brauchst mehr Action und willst dich mit anderen messen?<br/><br/></li>
+                    <strong>Dann bist du hier genau richtig!<br/><br/></strong>
+                    Senpai hilft dir, deine Lernziele auf spielerische Weise zu erreichen.<br/>
+                    Beginne dein Training indem du auf den Button unten rechts klickst.
                 </div>
             </div>
         </div>
