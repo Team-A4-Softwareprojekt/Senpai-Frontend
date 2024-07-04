1db45ec9ee934d6fc5c681821f7de7ae6e8d2ab7
kzuber93 <KevinZuber93@gmail.com>
2024-07-04 17:45:40 +0200

    [UPDATE] Added comments to card files

--- src/cards/userCard/UserCard.jsx
+++ src/cards/userCard/UserCard.jsx
@@ -1,5 +1,11 @@
 import styles from './UserCard.module.css';
 
+/**
+ * UserCard Component
+ * 
+ * This component is responsible for displaying a user's name and streak.
+ * It takes in two props, `userName` and `userStreak`, which represent the user's name and current streak respectively.
+ */
 function UserCard({ userName, userStreak }) {
   return (
     <div className= {styles.userCardDiv}>
